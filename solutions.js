dc383561e2f2d156f6f9aea5671c561361583024
granth2023 <grantharris38@gmail.com>
2023-12-01 10:43:30 -0500

    begin exercise 1

---
+++ solutions.js
@@ -0,0 +1,12 @@
+const album1 = {
+    title: 'Talking Heads',
+    albumDetails: {
+      released: new Date('September 16, 1977'),
+      label: 'Sire',
+      formats: ['LP']
+    }
+  };
+  
+  // Exercise 1:  Update the title property of album1 from 'Talking Heads' to 'Talking Heads - 77', then assign that property to a variable named title
+
+  
